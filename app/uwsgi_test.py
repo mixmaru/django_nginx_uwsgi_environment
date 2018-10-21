129780709bbdb8724f6a9158a477e8dc692190b0
maru <mix.touch777@gmail.com>
2018-10-21 15:37:48 +0900

    不要ファイル削除

--- app/uwsgi_test.py
+++
@@ -1,6 +0,0 @@
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    rvalue = b"Hello World\n"
-    rvalue += b"\tTest\n"
-    rvalue += b"\tJan/13/2017\n"
-    return [rvalue]
