2f299d364c69ae361b9f5c2c5154199e7a940118
maru <mix.touch777@gmail.com>
2018-11-20 15:35:29 +0900

    DB接続設定記入

--- app/project/project/settings.py
+++ app/project/project/settings.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'baz',
+        'USER': 'foo',
+        'PASSWORD': 'foobar',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
